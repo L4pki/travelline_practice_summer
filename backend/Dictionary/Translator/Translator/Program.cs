9e38f4816ba4deb6454692706d326ccdcd124e4d
L4pki <79217462+L4pki@users.noreply.github.com>
2024-07-07 11:11:20 +0300

    Взял за основу свой прошлый переводчик

---
+++ backend/Dictionary/Translator/Translator/Program.cs
@@ -0,0 +1,54 @@
+﻿using EnRuTranslator;
+class Programm
+{
+    static void Main()
+    {
+        Translator translator = new Translator();
+        bool isProgrammWorking = true;
+        while (isProgrammWorking)
+        {
+            Console.WriteLine("Введите код команды:\n (1)Добавить перевод\n (2)Удалить перевод\n (3)Изменить перевод\n (4)Перевести слово\n (5)Выйти");
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "1":
+                    Console.WriteLine("Введите слово и его перевод через запятую:");
+                    string[] input = Console.ReadLine().Split(',');
+                    if (input.Length < 2)
+                    {
+                        Console.WriteLine("Введены некорректные данные!");
+                        break;
+                    }
+                    translator.AddTranslation(input[0].Trim(), input[1].Trim());
+                    break;
+                case "2":
+                    Console.WriteLine("Введите слово для удаления:");
+                    string wordToRemove = Console.ReadLine();
+                    translator.RemoveTranslation(wordToRemove);
+                    break;
+                case "3":
+                    Console.WriteLine("Введите слово и новый перевод через запятую:");
+                    string[] changeInput = Console.ReadLine().Split(',');
+                    if (changeInput.Length < 2)
+                    {
+                        Console.WriteLine("Введены некорректные данные!");
+                        break;
+                    }
+                    translator.ChangeTranslation(changeInput[0].Trim(), changeInput[1].Trim());
+                    break;
+                case "4":
+                    Console.WriteLine("Введите слово для перевода:");
+                    string wordToTranslate = Console.ReadLine();
+                    Console.WriteLine(translator.Translate(wordToTranslate));
+                    break;
+                case "5":
+                    isProgrammWorking = false;
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная команда");
+                    break;
+            }
+        }
+    }
+}
---
+++ backend/Dictionary/Translator/Translator/Translator.cs
@@ -0,0 +1,86 @@
+﻿namespace EnRuTranslator
+{
+    class Translator
+    {
+        private Dictionary<string, string> _translations;
+
+        public Translator()
+        {
+            _translations = new Dictionary<string, string>();
+            LoadTranslationsFromFile("translations.txt");
+        }
+
+        public void AddTranslation(string word, string translation)
+        {
+            _translations[word] = translation;
+            _translations[translation] = word;
+            SaveTranslationsToFile("translations.txt");
+        }
+
+        public void RemoveTranslation(string word)
+        {
+            if (_translations.ContainsKey(word))
+            {
+                _translations.Remove(_translations[word]);
+                _translations.Remove(word);
+                SaveTranslationsToFile("translations.txt");
+            }
+            else
+            {
+                Console.WriteLine("Слова нет в словаре");
+            }
+        }
+
+        public void ChangeTranslation(string word, string newTranslation)
+        {
+            if (_translations.ContainsKey(word))
+            {
+                _translations.Remove(_translations[word]);
+                _translations.Remove(word);
+                _translations[word] = newTranslation;
+                _translations[newTranslation] = word;
+                SaveTranslationsToFile("translations.txt");
+            }
+            else
+            {
+                Console.WriteLine("Слова нет в словаре");
+            }
+        }
+
+        public string Translate(string word)
+        {
+            if (_translations.ContainsKey(word))
+            {
+                return _translations[word];
+            }
+            else
+            {
+                return "Слова нет в словаре";
+            }
+        }
+
+        private void LoadTranslationsFromFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                foreach (var line in lines)
+                {
+                    string[] parts = line.Split(',');
+                    _translations[parts[0]] = parts[1];
+                }
+            }
+        }
+
+        private void SaveTranslationsToFile(string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (var pair in _translations)
+                {
+                    writer.WriteLine(pair.Key + "," + pair.Value);
+                }
+            }
+        }
+    }
+}
