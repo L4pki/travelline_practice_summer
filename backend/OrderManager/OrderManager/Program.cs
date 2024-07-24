2289042c1736b0cf0c1297ba32ab9ad78f9dc616
L4pki <79217462+L4pki@users.noreply.github.com>
2024-07-24 03:42:31 +0300

    Добавил валидацию

--- backend/OrderManager/OrderManager/Program.cs
+++ backend/OrderManager/OrderManager/Program.cs
@@ -1,4 +1,4 @@
-﻿while (true)
+﻿while ( true )
 {
     string product = GetProduct();
     int count = GetCount();
@@ -18,33 +18,70 @@
 
 static string GetProduct()
 {
-    Console.Write( "Введите название товара: " );
-    return Console.ReadLine();
+    while ( true )
+    {
+        Console.Write( "Введите название товара: " );
+        string product = Console.ReadLine();
+        if ( !string.IsNullOrWhiteSpace( product ) )
+        {
+            return product;
+        }
+        Console.WriteLine( "Вы не указали название продукта!" );
+    }
 }
 
 static int GetCount()
 {
-    Console.Write( "Введите количество: " );
-    return int.Parse( Console.ReadLine() );
+    int count;
+    bool isValid;
+
+    do
+    {
+        Console.Write( "Введите количество: " );
+        string input = Console.ReadLine();
+        isValid = int.TryParse( input, out count );
+
+        if ( !isValid )
+        {
+            Console.WriteLine( "Введено некорректное значение. Пожалуйста, введите целое число." );
+        }
+    } while ( !isValid );
+    return count;
 }
 
 static string GetName()
 {
-    Console.Write( "Введите ваше имя: " );
-    return Console.ReadLine();
+    while ( true )
+    {
+        Console.Write( "Введите ваше имя: " );
+        string name = Console.ReadLine();
+        if ( !string.IsNullOrWhiteSpace( name ) )
+        {
+            return name;
+        }
+        Console.WriteLine( "Вы не указали имя!" );
+    }
 }
 
 static string GetAddress()
 {
-    Console.Write( "Введите адрес доставки: " );
-    return Console.ReadLine();
+    while ( true )
+    {
+        Console.Write( "Введите адрес доставки: " );
+        string addres = Console.ReadLine();
+        if ( !string.IsNullOrWhiteSpace( addres ) )
+        {
+            return addres;
+        }
+        Console.WriteLine( "Адрес не может быть пустым!" );
+    }
 }
 
 static bool GetConfirmation( string name, int count, string product, string address )
 {
     Console.WriteLine( $"Здравствуйте, {name}, вы заказали {count} {product} на адрес {address}, все верно? (да/нет)" );
     string confirmation = Console.ReadLine();
-    return confirmation.ToLower() == "да";
+    return string.Equals( confirmation, "да", StringComparison.OrdinalIgnoreCase );
 }
 
 static void ProcessOrder( string name, string product, int count, string address )
